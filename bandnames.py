f63e994b2e8e30a428cf93a5e6258c138a99cd61
Lily Pilblad <lily.pilblad@ppasshare.org>
2016-07-29 10:33:06 -0400

    this is my code to make a list of random bandnames

---
+++ bandnames.py
@@ -0,0 +1,10 @@
+from random import randint
+random.randint(0,9)
+#random_number = random.randint(0, 9)
+random_number1 = random.randint(0, 9)
+
+adjectives = ["funky", "psycho", "euphoric", "cool", "tranquil", "harsh", "fuzzy", "suspicious", "rocky", "smooth"]
+
+nouns = ["puppies", "butterflies", "mushrooms", "bears", "spiders", "peaches", "muffins", "newborns", "crocodiles", "lions"]
+
+#print(adjectives[random_number] + nouns[random_number1])
